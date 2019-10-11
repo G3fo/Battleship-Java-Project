2e3cc7b5b25516d1a1c4bc30d57d000361bb0832
g3fo <mabarreiros@gmail.com>
2019-10-11 11:24:31 -0300

    CreatePlayer creado

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -1,6 +1,11 @@
 package com.codeoftheweb.salvo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -12,43 +17,79 @@ import static java.util.stream.Collectors.toList;
 @RequestMapping("/api")
 public class SalvoController {
 
-    @Autowired
-    private GameRepository gameRepository;
-    @Autowired
-    private GamePlayerRepository gamePlayerRepository;
-    @Autowired
-    private PlayerRepository playerRepository;
+  @Autowired
+  private GameRepository gameRepository;
+  @Autowired
+  private GamePlayerRepository gamePlayerRepository;
+  @Autowired
+  private PlayerRepository playerRepository;
+  @Autowired
+  PasswordEncoder passwordEncoder;
 
-    @RequestMapping("/leaderboard")
-    public List<Object> getLeaderboard() {
-        return playerRepository
-                .findAll()
-                .stream()
-                .map(player -> player.toGameHistory())
-                .collect(Collectors.toList());
+  @RequestMapping("/leaderboard")
+  public List<Object> getLeaderboard() {
+    return playerRepository
+            .findAll()
+            .stream()
+            .map(player -> player.toGameHistory())
+            .collect(Collectors.toList());
+  }
+
+  //public Map<String, Object> getAllGames() {
+  //    Map<String, Object> dto = new HashMap<>();
+  //    dto.put("games", gameRepository
+  //           .findAll()
+  //           .stream()
+  //            .map(Game::createGameDTO)
+  //            .collect(toList()));
+  //    return dto;
+  //}
+
+  @RequestMapping("/games")
+  public Map<String, Object> getAllGames(Authentication authentication) {
+    Map<String, Object> dto = new HashMap<>();
+    if (!this.isGuest(authentication))
+      dto.put("player", playerRepository.findByUserName(authentication.getName()).createGameDTO_Player());
+    else
+      dto.put("player", "Guest");
+    dto.put("games", gameRepository
+            .findAll()
+            .stream()
+            .map(Game::createGameDTO)
+            .collect(Collectors.toList()));
+    return dto;
+  }
+
+  private boolean isGuest(Authentication authentication) {
+    return authentication == null || authentication instanceof AnonymousAuthenticationToken;
+  }
+
+  @RequestMapping(path = "/players", method = RequestMethod.POST)
+  public ResponseEntity<String> createUser(@RequestParam String username,
+                                                        @RequestParam String password) {
+
+    if (username.isEmpty()) {
+      return new ResponseEntity<>("No username given",HttpStatus.FORBIDDEN);
     }
-
-    @RequestMapping("/games")
-    public Map<String, Object> getAllGames() {
-        Map<String, Object> dto = new HashMap<>();
-        dto.put("games", gameRepository
-                .findAll()
-                .stream()
-                .map(Game::createGameDTO)
-                .collect(toList()));
-        return dto;
+    Player player = playerRepository.findByUserName(username);
+    if (player != null) {
+      return new ResponseEntity<>("Username already in use",HttpStatus.CONFLICT);
     }
+    Player newPlayer = playerRepository.save(new Player(username, passwordEncoder.encode(password)));
 
-    @RequestMapping("/game_view/{gamePlayerId}")
-    public Map<String,Object> getGameView(@PathVariable Long gamePlayerId) {
+    return new ResponseEntity<>(newPlayer.getUserName(), HttpStatus.CREATED);
+  }
 
-        GamePlayer gamePlayer = gamePlayerRepository.getOne(gamePlayerId);
+  @RequestMapping("/game_view/{gamePlayerId}")
+  public Map<String, Object> getGameView(@PathVariable Long gamePlayerId) {
 
-        Map<String, Object> dto = gamePlayer.getGame().createGameDTO();
-        dto.put("ships", gamePlayer.getShips().stream().map(Ship::createGameDTO_Ship));
-        dto.put("salvoes", gamePlayer.getGame().getGamePlayers().stream()
-                .flatMap(gp->gp.getSalvoes().stream().map(Salvo::createGameDTO_Salvo))
-                .collect(Collectors.toList()));
-        return dto;
-    }
+    GamePlayer gamePlayer = gamePlayerRepository.getOne(gamePlayerId);
+
+    Map<String, Object> dto = gamePlayer.getGame().createGameDTO();
+    dto.put("ships", gamePlayer.getShips().stream().map(Ship::createGameDTO_Ship));
+    dto.put("salvoes", gamePlayer.getGame().getGamePlayers().stream()
+            .flatMap(gp -> gp.getSalvoes().stream().map(Salvo::createGameDTO_Salvo))
+            .collect(Collectors.toList()));
+    return dto;
+  }
 }
