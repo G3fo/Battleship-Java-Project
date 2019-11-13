9e6573c1c1b8e77e695ed2655620ff6649db4431
Mateo Barreiros <matbarreiros@gmail.com>
2019-11-12 22:23:38 -0300

    Metodo para hacer el post de ships, y arreglado el fetch en main.js
    Falta debugear por que las lineas 173 y 174 de SalvoController hacen saltar un error 403

--- src/main/resources/static/web/scripts/main.js
+++ src/main/resources/static/web/scripts/main.js
@@ -1,69 +1,58 @@
-var gpId;
-var games;
-
-let shipArray = [];
-    let shipTypeArray = ["battleship", "patrol_boat", "destroyer", "submarine", "carrier"];
-  
-$.get("/api/games")
-    .done(function(json) {
-      gpId = json.gpid;
-      games = json
-    })
-    .fail(function() {
-      alert("Couldnt retrieve the game list!");
-    });
-
-
-
 function shipPOST() {
+  let shipTypeArray = [
+    "battleship",
+    "patrol_boat",
+    "destroyer",
+    "submarine",
+    "carrier"
+  ];
 
-    for (let i = 0; i < shipTypeArray.length; i++) {
-      let ship = document.querySelector("#" + shipTypeArray[i]);
-  
-      let width = ship.getAttribute("data-gs-width");
-      let height = ship.getAttribute("data-gs-height");
-      let x = parseInt(ship.getAttribute("data-gs-x"));
-      let y = parseInt(ship.getAttribute("data-gs-y"));
-      let locations = []
-  
-      if (height == 1) {
-        for (let j = 1; j <= width; j++) {
-          let currentX = x + j
-          let yLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(y);
-          locations.push(yLetter + currentX);
-        }
-      } else if (width == 1) {
-        for (let j = 0; j < height; j++) {
-          let currentY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.charAt(y + j);
-          let xPlusOne = x + 1;
-          locations.push(currentY + xPlusOne)
-        }
+  let shipArray = [];
+  let gpId = window.location.search.slice(-1);
+
+  for (let i = 0; i < shipTypeArray.length; i++) {
+    let ship = document.querySelector("#" + shipTypeArray[i]);
+
+    let width = ship.getAttribute("data-gs-width");
+    let height = ship.getAttribute("data-gs-height");
+    let x = parseInt(ship.getAttribute("data-gs-x"));
+    let y = parseInt(ship.getAttribute("data-gs-y"));
+    let locations = [];
+
+    if (height == 1) {
+      for (let j = 1; j <= width; j++) {
+        let currentX = x + j;
+        let yLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(y);
+        locations.push(yLetter + currentX);
       }
-      shipArray.push({
-        shipType: shipTypeArray[i].toUpperCase(),
-        locations: locations
-      })
+    } else if (width == 1) {
+      for (let j = 0; j < height; j++) {
+        let currentY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(y + j);
+        let xPlusOne = x + 1;
+        locations.push(currentY + xPlusOne);
+      }
     }
-  
-  
-      fetch('/api/games/players/1/ships', {
-        method: 'POST',
-        body: JSON.stringify(shipArray),
-        headers: {'Content-Type': 'application/json'},
-        credentials: 'same-origin'
-      })
-          .then(function (response) {
-            response.status
-            if (response.status == 201) {
-  
-              console.log("Ships Fetched");
-            } else {
-              console.log("invalid")
-            }
-          })
-          .catch(function (error) {
-            console.log(error);
-          })
-  
+    shipArray.push({
+      type: shipTypeArray[i].toUpperCase(),
+      locations: locations
+    });
   }
-  
+
+  fetch("/api/games/players/" + gpId + "/ships", {
+    method: "POST",
+    body: JSON.stringify(shipArray),
+    headers: { "Content-Type": "application/json" },
+    credentials: "same-origin"
+  })
+    .then(function(response) {
+      response.status;
+      if (response.status == 201) {
+        console.log("Ships Fetched");
+      } else {
+        console.log("invalid");
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
