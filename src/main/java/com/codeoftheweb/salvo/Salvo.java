9ef959c9dc8b22c25acbe80acd2a0fd942c05664
Mateo Barreiros <matbarreiros@gmail.com>
2019-12-01 14:42:02 -0300

    Implementados los metodos get sinks, gethits, y getopponent gameplayer, y añadidos al dto de salvoes

--- src/main/java/com/codeoftheweb/salvo/Salvo.java
+++ src/main/java/com/codeoftheweb/salvo/Salvo.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 public class Salvo {
@@ -43,12 +44,43 @@ public class Salvo {
     return salvoLocations;
   }
 
+  private GamePlayer getOpponentGamePlayer(){
+    GamePlayer opponentGamePlayer = this.getGamePlayer().getGame().getGamePlayers().stream().filter(gamePlayer -> gamePlayer.getGamePlayerId() != this.getGamePlayer().getGamePlayerId()).findFirst().orElse(null);
+    return opponentGamePlayer;
+  }
+
+  private List<String> getHits(){
+    List<String> hitShips;
+    if (this.getOpponentGamePlayer() != null)
+
+      hitShips = this.getSalvoLocations().stream().filter(location -> getOpponentGamePlayer().getShips().stream().anyMatch(ship -> ship.getLocations().contains(location))).collect(Collectors.toList());
+
+    else
+      hitShips = null;
+    return hitShips;
+  }
+
+  private List<Map<String,Object>> getSinks(){
+    List<Map<String,Object>> sinkShips;
+    List<String> allEnemyLocations = new ArrayList<>();
+    this.getGamePlayer().getSalvoes().stream().filter(e -> e.getTurn()<= this.getTurn()).forEach(x -> allEnemyLocations.addAll(x.getSalvoLocations()));
+    if (this.getOpponentGamePlayer() != null)
+
+      sinkShips = this.getOpponentGamePlayer().getShips().stream().filter(ship -> allEnemyLocations.containsAll(ship.getLocations())).map(Ship::createGameDTO_Ship).collect(Collectors.toList());
+
+    else
+      sinkShips = null;
+    return sinkShips;
+  }
+
   public Map<String, Object> createGameDTO_Salvo (){
     Map<String, Object> dto = new LinkedHashMap<>();
     dto.put("turn", this.getTurn());
     dto.put("player", this.getGamePlayer().getPlayer().getUserName());
     dto.put("game_player_id", this.getGamePlayer().getGamePlayerId());
     dto.put("locations", this.getSalvoLocations());
+    dto.put("hits", this.getHits());
+    dto.put("sinks", this.getSinks());
     return dto;
   }
 }
