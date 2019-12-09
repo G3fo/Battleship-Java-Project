ebef7d435e5c231ec732e036568dbe820794e309
g3fo <matbarreiros@gmail.com>
2019-12-09 12:21:25 -0300

    Update games.js

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -9,15 +9,15 @@ var app = new Vue({
   },
   methods: {
     returnToGame(id) {
-      window.location.href = "http://localhost:8080/web/game.html?gp=" + id;
+      window.location.href = "game.html?gp=" + id;
     },
     joinGame(gameid) {
       $.post("/api/game/" + gameid + "/players")
-        .done(function(json) {
+        .done(function (json) {
           window.location.href =
-            "http://localhost:8080/web/game.html?gp=" + json.gpid;
+            "game.html?gp=" + json.gpid;
         })
-        .fail(function(jqXHR, textStatus) {
+        .fail(function (jqXHR, textStatus) {
           showOutput("Failed: " + textStatus);
           alert("Could not join the game!");
         });
@@ -27,7 +27,7 @@ var app = new Vue({
 Vue.config.devtools = true;
 
 $.get("/api/games")
-  .done(function(games) {
+  .done(function (games) {
     gamesJSON = games;
     var date;
     gamesJSON.games.forEach(game => {
@@ -37,65 +37,65 @@ $.get("/api/games")
 
     app.vueGames = gamesJSON;
   })
-  .fail(function(jqXHR, textStatus) {
+  .fail(function (jqXHR, textStatus) {
     showOutput("Failed: " + textStatus);
   });
 $.get("/api/leaderboard")
-  .done(function(scores) {
+  .done(function (scores) {
     scores.sort((a, b) => b["total"] - a["total"]);
     app.vueScores = scores;
     console.log(app.vueScores);
   })
-  .fail(function(jqXHR, textStatus) {
+  .fail(function (jqXHR, textStatus) {
     showOutput("Failed: " + textStatus);
   });
 
 function logout() {
-  $.post("/api/logout").done(function() {
+  $.post("/api/logout").done(function () {
     console.log("logged out");
   });
 }
 
 function login() {
   $.post("/api/login", {
-    username: document.getElementById("loginEmail").value,
-    password: document.getElementById("loginPassword").value
-  })
-    .done(function(username) {
-      (username = document.getElementById("loginEmail").value),
-        swal("Logged in!", "Welcome back " + username + "!", "success").then(
-          function() {
-            window.location.reload();
-          }
-        );
+      username: document.getElementById("loginEmail").value,
+      password: document.getElementById("loginPassword").value
     })
-    .fail(function() {
+    .done(function (username) {
+      (username = document.getElementById("loginEmail").value),
+      swal("Logged in!", "Welcome back " + username + "!", "success").then(
+        function () {
+          window.location.reload();
+        }
+      );
+    })
+    .fail(function () {
       swal("Whoops", "Incorrect username or password", "error");
     });
 }
 
 function signup() {
   $.post("/api/players", {
-    username: document.getElementById("loginEmail").value,
-    password: document.getElementById("loginPassword").value
-  })
-    .done(function() {
-      (username = document.getElementById("loginEmail").value),
-        swal("Signed up!", "").then(function() {
-          login();
-        });
+      username: document.getElementById("loginEmail").value,
+      password: document.getElementById("loginPassword").value
     })
-    .fail(function() {
+    .done(function () {
+      (username = document.getElementById("loginEmail").value),
+      swal("Signed up!", "").then(function () {
+        login();
+      });
+    })
+    .fail(function () {
       swal("Whoops", "Incorrect sign up!", "error");
     });
 }
 
 function createGame() {
   $.post("/api/games")
-    .done(function(json) {
+    .done(function (json) {
       window.location.href = "game.html?gp=" + json.gpid;
     })
-    .fail(function() {
+    .fail(function () {
       swal("Whoops", "Couldn't create game!", "error");
     });
 }
